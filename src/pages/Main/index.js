6ad362de63756d3e123244ef9f88876268f614a3
Lucas Domingues <lucasdomi@globo.com>
2019-08-03 01:37:50 -0300

    feat: create main page

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,5 +1,20 @@
 import React from 'react';
-import { Title } from './styles';
+import { Container, Form, SubmitButton } from './styles';
+import { FaGithubAlt, FaPlus } from 'react-icons/fa';
 export default function Main() {
-  return <Title>Main</Title>;
+  return (
+    <Container>
+      <h1>
+        <FaGithubAlt />
+        Repositórios
+      </h1>
+
+      <Form onSubmit={() => {}}>
+        <input type="text" placeholder="Adicionar repositório" />
+        <SubmitButton>
+          <FaPlus color="#fff" size="14" />
+        </SubmitButton>
+      </Form>
+    </Container>
+  );
 }

0dc9d24a296f26b99d1dbc99388000af9f4b5d2f
Lucas Domingues <lucasdomi@globo.com>
2020-05-02 19:55:34 -0300

    feat: styling details page

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
-import { useRouteMatch } from 'react-router-dom';
+import { useRouteMatch, Link } from 'react-router-dom';
+import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
+import { Header, RepositoryInfo, Issues } from './styles';
+import logoImg from '../../assets/logo.svg';
 
 interface RepositoryParams {
   repository: string;
@@ -7,7 +10,56 @@ interface RepositoryParams {
 
 const Repository: React.FC = () => {
   const { params } = useRouteMatch<RepositoryParams>();
-  return <h1>Repository: {params.repository}</h1>;
+  return (
+    <>
+      <Header>
+        <img src={logoImg} alt="GitHub Explorer" />
+        <Link to="/">
+          <FiChevronLeft size={16} />
+          Voltar
+        </Link>
+      </Header>
+
+      <RepositoryInfo>
+        <header>
+          <img
+            src="https://avatars1.githubusercontent.com/u/19497199?s=460&u=0ca2d29d37251e31cc0853ace54e26bd307f4431&v=4"
+            alt="Lucas Domingues"
+          />
+          <div>
+            <strong>nome</strong>
+            <p>descricao</p>
+          </div>
+        </header>
+
+        <ul>
+          <li>
+            <strong>10</strong>
+            <span>Starts</span>
+          </li>
+          <li>
+            <strong>10</strong>
+            <span>Forks</span>
+          </li>
+          <li>
+            <strong>10</strong>
+            <span>Issues abertas</span>
+          </li>
+        </ul>
+      </RepositoryInfo>
+
+      <Issues>
+        <Link to="asasda">
+          <div>
+            <strong>asdfsad</strong>
+            <p>adsfasdf</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </Link>
+      </Issues>
+    </>
+  );
 };
 
 export default Repository;
